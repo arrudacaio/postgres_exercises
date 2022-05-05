55bce00bf8ff1523b73367127c27ccbaefd6f7e3
Caio Arruda <caio.arruda@pagar.me>
2022-05-05 20:10:59 -0300

    Adds query 4

--- simplesqlqueries.sql
+++ simplesqlqueries.sql
@@ -7,3 +7,6 @@ select fc.name, fc.membercost from cd.facilities as fc;
 -- Q3
 
 select fc.* from cd.facilities as fc where fc.membercost > 0;
+
+-- Q4
+select fc.facid, fc.name, fc.membercost, fc.monthlymaintenance from cd.facilities as fc where fc.membercost > 30;
